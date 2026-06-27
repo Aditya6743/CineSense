7ad82b45ff0c2be0e3f496306c98cf474c479518
Aditya6743 <adityatripathi6743@gmail.com>
2026-06-27 15:48:28 +0530

    feat: merge movie and credits datasets

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
+# Load datasets
 movies = pd.read_csv("data/tmdb_5000_movies.csv")
 credits = pd.read_csv("data/tmdb_5000_credits.csv")
 
-print(movies.shape)
-print(credits.shape)
+# Merge datasets using the movie title
+movies = movies.merge(credits, on="title")
 
+# Check the new dataset
+print(movies.shape)
 print(movies.head())

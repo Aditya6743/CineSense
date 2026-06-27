b93628bfcf0e7f3fbf044faa793cfdeea1179ba6
Aditya6743 <adityatripathi6743@gmail.com>
2026-06-27 16:31:32 +0530

    feat: implement movie similarity engine

--- train.py
+++ train.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import ast
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+ps = PorterStemmer()
 
 # Load datasets
 movies = pd.read_csv("data/tmdb_5000_movies.csv")
@@ -11,15 +16,75 @@ movies = movies.merge(credits, on="title")
 # Keep useful columns
 movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
 
-# Function to extract names
+# Remove null values
+movies.dropna(inplace=True)
+
 def convert(obj):
     L = []
     for i in ast.literal_eval(obj):
         L.append(i['name'])
     return L
 
-# Clean genres
-movies['genres'] = movies['genres'].apply(convert)
+def convert3(obj):
+    L = []
+    counter = 0
+    for i in ast.literal_eval(obj):
+        if counter != 3:
+            L.append(i['name'])
+            counter += 1
+        else:
+            break
+    return L
 
-# Show result
-print(movies[['title','genres']].head())
+def fetch_director(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+            break
+    return L
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(convert3)
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+movies['genres'] = movies['genres'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['keywords'] = movies['keywords'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['cast'] = movies['cast'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['crew'] = movies['crew'].apply(lambda x:[i.replace(" ","") for i in x])
+
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+
+new_df = movies[['movie_id','title','tags']]
+
+new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x))
+new_df['tags'] = new_df['tags'].apply(lambda x: x.lower())
+
+def stem(text):
+    y = []
+    for i in text.split():
+        y.append(ps.stem(i))
+    return " ".join(y)
+
+new_df['tags'] = new_df['tags'].apply(stem)
+
+# ================= MACHINE LEARNING =================
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+
+vectors = cv.fit_transform(new_df['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
+
+print("Vector Shape:", vectors.shape)
+print("Similarity Shape:", similarity.shape)
+
+# Example
+print("\nSimilarity between Avatar and Avatar:")
+print(similarity[0][0])
+
+print("\nSimilarity between Avatar and Pirates:")
+print(similarity[0][1])
